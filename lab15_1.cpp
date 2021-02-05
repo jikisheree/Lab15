c3041c6de9d30da7d90353ac42c6d1b016fb0336
jikisheree <srikeaw2020@gmail.com>
2021-02-05 16:27:17 +0700

    ดรืรห้

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -32,3 +32,45 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a+i);
+        if((i+1)%M==0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *b,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(b+i) = (rand()%101)/100.;
+    }
+}
+
+void findRowSum(const double *a,double *b,int N,int M){
+    int p=0;
+    double sum;
+    for(int i = 0; i < (N*M); i++){
+        sum +=*(a+i);
+        if((i+1)%M == 0){
+        *(b+p) = sum;
+        sum = 0;
+        b += 1;
+        }
+    }
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+    double sum;
+    for(int i = 0; i<M; i++){
+    if(N==1) *(b+i)=*(a+i);
+    else{
+            sum=*(a+i);
+            for(int p=i+M;p<(N*M);p+=M){
+            sum+=*(a+p);
+            }
+            *(b+i)=sum;
+        }
+    }
+}
